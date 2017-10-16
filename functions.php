ab6eb9f4be0b459de23f76d803b19f7f13414a16
marciojc <marciojc@gmail.com>
2017-10-16 23:51:51 +0100

    Start Twenty Seventeen theme

---
+++ functions.php
@@ -0,0 +1,102 @@
+<?php
+
+if ( ! class_exists( 'Timber' ) ) {
+	add_action( 'admin_notices', function() {
+		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php') ) . '</a></p></div>';
+	});
+
+	add_filter('template_include', function($template) {
+		return get_stylesheet_directory() . '/static/no-timber.html';
+	});
+
+	return;
+}
+
+Timber::$dirname = array('templates', 'views');
+
+class StarterSite extends TimberSite {
+
+	function __construct() {
+		add_theme_support( 'post-formats' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'menus' );
+		add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+		add_action( 'init', array( $this, 'register_post_types' ) );
+		add_action( 'init', array( $this, 'register_taxonomies' ) );
+		parent::__construct();
+	}
+
+	function register_post_types() {
+		//this is where you can register custom post types
+	}
+
+	function register_taxonomies() {
+		//this is where you can register custom taxonomies
+	}
+
+	function add_to_context( $context ) {
+		$context['foo'] = 'bar';
+		$context['stuff'] = 'I am a value set in your functions.php file';
+		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
+		$context['menu'] = new TimberMenu();
+		$context['site'] = $this;
+		return $context;
+	}
+
+	function myfoo( $text ) {
+		$text .= ' bar!';
+		return $text;
+	}
+
+	function add_to_twig( $twig ) {
+		/* this is where you can add your own functions to twig */
+		$twig->addExtension( new Twig_Extension_StringLoader() );
+		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
+		// wordpress
+		$twig->addFunction( new Timber\Twig_Function( 'is_home', 'is_home' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'is_front_page', 'is_front_page' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'has_custom_header', 'has_custom_header' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'is_single', 'is_single' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'is_page', 'is_page' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'is_active_sidebar', 'is_active_sidebar' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'has_nav_menu', 'has_nav_menu' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'have_posts', 'have_posts' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'is_customize_preview', 'is_customize_preview' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'page_for_posts', 'page_for_posts' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'get_option', 'get_option' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'sprintf', 'sprintf' ) );
+		// theme
+		$twig->addFunction( new Timber\Twig_Function( 'twentyseventeen_get_svg', 'twentyseventeen_get_svg' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'twentyseventeen_is_frontpage', 'twentyseventeen_is_frontpage' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'twentyseventeen_edit_link', 'twentyseventeen_edit_link' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'twentyseventeen_panel_count', 'twentyseventeen_panel_count' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'twentyseventeen_front_page_section', 'twentyseventeen_front_page_section' ) );
+		$twig->addFunction( new Timber\Twig_Function( 'dd', 'dd' ) );
+		//
+		$twig->addFilter('ratio', new Twig_SimpleFilter( 'ratio', 'ratio' ) );
+
+		return $twig;
+	}
+
+}
+
+function dd($values) {
+	echo "<pre>";
+	print_r($values);
+	echo "</pre>";
+	die();
+}
+
+function ratio($thumbnail){
+	return esc_attr($thumbnail[2] / $thumbnail[1] * 100);
+}
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/theme-functions.php';
+require get_template_directory() . '/inc/twig-functions.php';
+
+new StarterSite();
