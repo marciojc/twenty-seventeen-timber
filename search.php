652c662feea070f8957d41a74eeb122c4de2a844
marciojc <marciojc@gmail.com>
2017-10-25 01:04:01 +0100

    Templase Timber v1.0.0.0

--- search.php
+++ search.php
@@ -12,7 +12,7 @@
 $templates = array( 'search.twig', 'archive.twig', 'index.twig' );
 $context = Timber::get_context();
 
-$context['title'] = 'Search results for '. get_search_query();
 $context['posts'] = Timber::get_posts();
+$context['sidebar'] = Timber::get_sidebar('sidebar.php');
 
 Timber::render( $templates, $context );
